459fefdf5285112abd430363a148de3f41c8d0e6
nehssss <562410105@qq.com>
2023-04-29 20:39:29 -0500

    init currency-conversion-service

---
+++ src/test/java/com/haitaos/microservices/currencyconversionservice/CurrencyConversionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haitaos.microservices.currencyconversionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyConversionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
